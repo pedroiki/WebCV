d7c28d2229787063441106e563878dfb9d968f82
pedroiki <pedrobzg@gmail.com>
2023-12-15 00:25:59 +0000

    WebCV

--- main.js
+++ main.js
@@ -1,81 +1,9 @@
-console.log("oi")
+// ... (código JavaScript existente) ...
 
-//list
-//  [] = array
-// {}  = object , functions
-// target blank : open a new tab
-let projects = [{
-    imageUrl: "images/01.jpg",
-    link: "https://pedroiki.github.io/costeira.praia/",
-  },
-  {
-    imageUrl: "images/02.jpg",
-    link: "https://pedroiki.github.io/login-firebase/",
-  },
-  {
-    imageUrl: "images/03.jpg",
-    link: "https://pedroiki.github.io/retro-90/",
-  },
-  {
-    imageUrl: "images/04.jpg",
-
-    link: "https://pedroiki.github.io/mobile-tag/",
-  },
-  {
-    imageUrl: "images/05.jpg",
-
-    link: "https://pedroiki.github.io/Comics_eCommerce-beta1.0/",
-  },
-  {
-    imageUrl: "images/06.jpg",
-
-    link: "https://pedroiki.github.io/tomato_game2016/",
-  },
-  {
-    imageUrl: "images/07.jpg",
-
-    link: "https://pedroiki.github.io/React_app/",
-  },
-
-  {
-    imageUrl: "images/08.jpg",
-
-    link: "https://pedroiki.github.io/Mikel_Therapist/",
-  },
-
-    {
-      imageUrl: "images/09.jpg",
-
-      link: "https://pedroiki.github.io/blog_prototype/",
-    },
-
-        {
-          imageUrl: "images/010.jpg",
-
-          link: "https://pedroiki.github.io/blog_html_css/",
-        },
-
-
-
-]
-
-window.onload = function() {
-  loadProjects()
-}
-
-function loadProjects() {
-  var container = document.querySelector(".projects")
-
-  projects.forEach((project) => {
-    let image = `
-         <a href="${project.link}" target="_blank">
-           <img src="${project.imageUrl}" class="project-image">
-           </img>
-         </a>`
-    let imageContainer = document.createElement("div")
-    imageContainer.setAttribute('class', 'image-container')
-    imageContainer.innerHTML = image
-    container.appendChild(imageContainer)
+// Adicione esta parte para o painel de habilidades
+document.getElementById("flip").addEventListener("click", function () {
+    document.getElementById("panel").classList.toggle("show-panel");
   });
-
-}
+  
+  // ... (código JavaScript existente) ...
+  
